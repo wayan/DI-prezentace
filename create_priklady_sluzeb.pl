481be9929e786c92b6a8645ddfed7d77f9b26a7a
Roman Daniel <roman.daniel@davosro.cz>
2016-10-19 19:20:38 +0200

    dalsi vyvoj

---
+++ create_priklady_sluzeb.pl
@@ -0,0 +1,25 @@
+use common::sense;
+use List::Util qw(min);
+
+my $rows_per_slide = shift;
+
+my @rows = <>;
+my $title = "Příklady služeb";
+my $i = 1;
+for(my $from = 0; $from < @rows; $from+=$rows_per_slide){
+    $i++ ;
+    my $to = min($from + $rows_per_slide, @rows - 1);
+    my $the_rows = join '', @rows[ $from .. $to ];
+
+print <<"END_TXT";	
+    <section class="slide">
+        <h2>$title</h2>
+
+        <pre>$the_rows</pre>
+    </section>
+END_TXT
+
+    $title = "Příklady služeb díl $i.";
+}
+
+# vim: expandtab:shiftwidth=4:tabstop=4:softtabstop=0
